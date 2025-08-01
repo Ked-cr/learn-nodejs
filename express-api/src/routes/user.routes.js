75879276f0a58ab6a4300a79eaa26b327c2f4795
EkoRamdn <ekorama.mail@gmail.com>
2025-08-01 22:51:31 +0700

    feat(routes): add auth and user routing structure

---
+++ express-api/src/routes/user.routes.js
@@ -0,0 +1,10 @@
+// src/routes/user.routes.js
+import express from "express";
+import { getUsers, getUserById } from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+router.get("/", getUsers);
+router.get("/:id", getUserById);
+
+export default router;
